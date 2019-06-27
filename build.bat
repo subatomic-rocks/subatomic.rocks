526169f057a47472b9e88c821d4837f2c9a1ad4a
Matheus Pratta <matpratta@gmail.com>
2019-06-27 16:13:29 -0300

    Removed WordPress-specific code

--- build.bat
+++ build.bat
@@ -1,1 +1,10 @@
-sass --watch --no-source-map scss/main.scss:css/main.css
+@ECHO OFF
+
+REM COPY OVER SOME OF SOURCE
+REM TODO: Convert this to Webpack
+echo f | xcopy /F /Y /E src\index.html dist\index.html
+echo d | xcopy /F /Y /E src\js dist\js
+echo d | xcopy /F /Y /E src\img dist\img
+
+REM Compile SCSS
+sass --no-source-map src/scss/main.scss:dist/css/main.css

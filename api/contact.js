faa74d96c8c978d51b6ae4fa60d6d4ddb2c562ab
Matheus Pratta <matpratta@gmail.com>
2019-07-08 00:29:56 -0300

    Contact Test

---
+++ api/contact.js
@@ -0,0 +1,22 @@
+// Imports the Sendgrid API and sets API key
+const sendgridMail = require('@sendgrid/mail')
+sendgridMail.setApiKey(process.env.sendgrid_key)
+
+module.exports = (req, res) => {
+  let { name, email, phone, message } = req.body
+
+  let email = {
+    to: 'oi@subatomic.rocks',
+    from: 'oi@subatomic.rocks',
+    subject: `Contact: ${name}`,
+    text: `From: ${name}, ${email}
+    Phone: ${phone}
+    Message:
+    ${message}`
+  }
+
+  let response = sendgridMail.send(email)
+
+  console.log('Contact', response, req.body, email)
+  res.send('Sent.')
+}
